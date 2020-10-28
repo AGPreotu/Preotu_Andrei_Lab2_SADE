6d0effab4fdd3ff1dfbc475f20985d73ebc2e023
Andrei Preotu <andrei.preotu.g@gmail.com>
2020-10-28 20:35:47 +0200

    Lab4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Preotu_Andrei_Lab2_SADE.Models;
+using Microsoft.EntityFrameworkCore;
+using Preotu_Andrei_Lab2_SADE.Data;
+using Preotu_Andrei_Lab2_SADE.Models.LibraryViewModels;
 
 namespace Preotu_Andrei_Lab2_SADE.Controllers
 {
@@ -13,10 +16,29 @@ namespace Preotu_Andrei_Lab2_SADE.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+     /*   public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        }*/
 
         public IActionResult Index()
         {
---
+++ Models/LibraryViewModels/OrderGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Preotu_Andrei_Lab2_SADE.Models.LibraryViewModels
+{
+    public class OrderGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? OrderDate { get; set; }
+        public int BookCount { get; set; }
+
+    }
+}
